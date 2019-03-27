4c6ea424e389f12b0d0ac2a6b63ed9e123cbb8d1
Hubert <huberthflin@gmail.com>
2019-03-26 20:40:54 -0400

    first commit

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+// import store from './store';
+// import './styles.css';
+
+render(<App />, document.getElementById('contents'));
